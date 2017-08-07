5aa9f42dcb143ff1a5c7dfc8eed96f8d9529d98b
Glen Young <extractofcactus@icloud.com>
2017-08-07 17:37:41 +0100

    film post and put added

--- controllers/films.js
+++ controllers/films.js
@@ -14,4 +14,18 @@ filmsRouter.get('/:id', function(req, res) {
   res.json({data: films[req.params.id]});
 });
 
+filmsRouter.put('/:id', function(req, res) {
+  films[req.params.id] = req.body;
+  res.json({data: films});
+});
+
+filmsRouter.post('/', function(req, res) {
+  films.push(req.body);
+  res.json({data: films});
+});
+
+filmsRouter.delete('/:id', function(req, res) {
+  
+})
+
 module.exports = filmsRouter;
